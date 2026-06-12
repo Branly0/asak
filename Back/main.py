d1aade215aeece0ecaa2536951200a7196cfb28a
branly0 <nkasibranly01@gmail.com>
2026-06-12 20:32:20 +0100

    dman

--- Back/main.py
+++ Back/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from apis.auth import router as auth_router
 from apis.assessment import router as assessment_router
 
 app = FastAPI()
 app.include_router(auth_router)
 app.include_router(assessment_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000","*","http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
