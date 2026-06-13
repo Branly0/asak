4aecb4a5f32b027332c20d7d32d452e08ae1174b
branly0 <nkasibranly01@gmail.com>
2026-06-13 08:38:51 +0100

    deploy finale hope 5

--- Back/main.py
+++ Back/main.py
@@ -8,7 +8,7 @@ app.include_router(auth_router)
 app.include_router(assessment_router)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","*","http://localhost:5173"],
+    allow_origins=["http://localhost:3000","*","http://localhost:5173","*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
